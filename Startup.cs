0fe308ddf4c4a24ad2928d99648477b0d7aa61c7
Pier Yos <khmaipapi@yahoo.com>
2019-07-25 15:58:16 -0400

    Added SQLite

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using reimbursement_server_side.Models;
 
 namespace reimbursement_server_side
 {
@@ -26,6 +29,13 @@ namespace reimbursement_server_side
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<ApplicationDbContext>(options => 
+            {
+                if(!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    options.UseSqlite("Data Source=reimbursement.db");
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
